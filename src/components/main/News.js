1d64c5be98de69df829489e843affee40c41bc2f
natherma <65506524+natherma@users.noreply.github.com>
2022-01-20 22:19:09 +0530

    APi key generated

---
+++ src/components/main/News.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Newitem from './Newitem.js';
+
+export default class News extends Component {
+  render() {
+    return (
+       <Newitem /> 
+    );
+  }
+}
